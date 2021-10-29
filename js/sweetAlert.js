20a4c7c18350997a8883c05b2acc74ffa89855e9
Farid Espadas <ivanfaridespadas@gmail.com>
2021-10-29 03:56:53 -0500

    agregando alerta de coockies en el index

---
+++ js/sweetAlert.js
@@ -0,0 +1,7 @@
+Swal.fire({
+    title: 'Coockies',
+    text: 'En este sitio usamos coockies para mejorar su experiencia de navegación',
+    confirmButtonText: "Aceptar", 
+    confirmButtonColor: 'rgb(75, 172, 37)'
+});
+
