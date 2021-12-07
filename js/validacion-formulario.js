ed6ae66911474f167316be12fe3b73faa18896c4
IFEE09 <ivanfaridespadas@gmail.com>
2021-12-07 02:33:10 -0600

    terminando de validar formulario con javascript

--- js/validacion-formulario.js
+++ js/validacion-formulario.js
@@ -1,77 +1,100 @@
-let password = document.getElementById('password').value;
-let password_repeat = document.getElementById('password-repeat').value;
-
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input'); //creando un array de los formularios
 
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{1,50}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
+	password: /^.{8,50}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 };
 
-//Valida que no este vacio
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault();
-});
+const campos = {
+    nombre: false,
+    apellido: false,
+    correo: false,
+    password: false
+};
 
 const validarFormulario = (event) => {
     
     switch(event.target.name){
+        
         case 'name':
             if(expresiones.nombre.test(event.target.value)){
                 document.getElementById('nombre').style.color = "green";
+                campos['nombre'] = true;
             } else {
                 document.getElementById('nombre').style.color = "red"; 
+                campos['nombre'] = false;
             }
         break;
+
         case 'last-name':
             if(expresiones.nombre.test(event.target.value)){
                 document.getElementById('apellido').style.color = "green";
+                campos['apellido'] = true;
             } else {
                 document.getElementById('apellido').style.color = "red"; 
+                campos['apellido'] = false;
             }
         break;
+
         case 'email':
             if(expresiones.correo.test(event.target.value)){
                 document.getElementById('correo').style.color = "green";
+                campos['correo'] = true;
             } else {
                 document.getElementById('correo').style.color = "red"; 
+                campos['correo'] = false;
             }
         break;
+
         case 'password':
+            validarPassword2();
             if(expresiones.password.test(event.target.value)){
                 document.getElementById('password').style.color = "green";
+                campos['password'] = true;
             } else {
                 document.getElementById('password').style.color = "red"; 
+                campos['password'] = false;
             }
         break;
+
         case 'password-repeat':
-            if(expresiones.password.test(event.target.value)){
-                document.getElementById('password-repeat').style.color = "green";
-            } else {
-                document.getElementById('password-repeat').style.color = "red"; 
-            }
+            validarPassword2();
         break;
     }
 };
 
+const validarPassword2 = () => {
 
+    const password1 = document.getElementById('password');
+    const password2 = document.getElementById('password-repeat');
+
+    if(password1.value !== password2.value){
+        document.getElementById('password-repeat').style.color = "red";
+        campos['password'] = false;
+    } else {
+        document.getElementById('password-repeat').style.color = "green";
+        campos['password'] = true;
+    }
+}
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarFormulario);
     input.addEventListener('blur', validarFormulario);
 });
 
-function validar_Formulario(){
-    if (password != password_repeat) {
-        Swal.fire({
-            icon: 'error',
-            text: 'Los password deben de ser iguales.',
-        })
+//Valida que no este vacio y que sea correcto
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const enviado = document.getElementById('enviado');
+    const no_enviado = document.getElementById('no-enviado');
+
+    if(campos.nombre && campos.apellido && campos.password && campos.correo ) {
+        formulario.reset();
     }
-}
 
-
+});
