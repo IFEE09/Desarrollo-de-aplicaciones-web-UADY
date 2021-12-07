5039dacafbd0737a6839051bf162d40c101cf6ad
IFEE09 <ivanfaridespadas@gmail.com>
2021-12-07 03:35:36 -0600

    comenzando validacion por incio de sesion

--- js/sweetAlert.js
+++
@@ -1,6 +0,0 @@
-// Swal.fire({
-//     title: 'Coockies',
-//     text: 'En este sitio usamos coockies para mejorar su experiencia de navegación',
-//     confirmButtonText: "Aceptar", 
-//     confirmButtonColor: 'rgb(75, 172, 37)'
-// });
---
+++ js/validacion-login.js
@@ -0,0 +1,10 @@
+let sesion = false;
+
+const sesion = document.getElementById('inicio-sesion');
+
+if(sesion == false){
+    window.location = 'login.html';
+} else {
+    window.location = 'index.html';
+}
+
