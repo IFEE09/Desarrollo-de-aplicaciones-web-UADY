9e71953cd64871bd8094d1a64cbfee908ceea5ea
IFEE09 <ivanfaridespadas@gmail.com>
2021-12-07 20:28:51 -0600

    agregando paginas y api

---
+++ Proyecto/README.md
@@ -0,0 +1,13 @@
+# Desarrollo de Aplicaciones Web - UADY
+
+Proyecto Final: Una tienda en linea para productores y artesanos de México
+
+Repositorio principal de la materia de Desarrollo de Aplicaciones Web
+
+Sitio de prueba para el proyecto: <a href="http://nulleaks.com/" target="_blank">Mercados México</a> (sin certificado SSL)
+
+# Integrantes
+| Ivan Farid Espadas Escalante| Manuel Jasaelle Peraza Soberanis|
+| ----- | ---- |
+|![ivan](https://github.com/IFEE09/Desarrollo_Web/blob/main/Integrantes/Ivan.jpeg)|![manuel](https://github.com/IFEE09/Desarrollo_Web/blob/main/Integrantes/Manuel.jpeg)|
+|[IFEE09](https://github.com/IFEE09)|[jasperaza](https://github.com/jasperaza)|
---
+++ Proyecto/js/validacion-login.js
@@ -0,0 +1,127 @@
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+let sesion = false;
+
+const validar = () => {
+
+}
+
+const credenciales = {
+    email: 'usuario@gmail.com',
+    password: 'usuario1234',
+    super_email: 'root@gmail.com',
+    super_password: 'admin1234',
+    password_good: /^.{8,28}$/, // 4 a 12 digitos.
+	email_good: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+}
+
+const campos = {
+    correo: false,
+    password: false,
+    super_correo: false,
+    super_password: false
+};
+
+//Valida el formulario, falta validar el porque cuando pones el correcto da error. 
+const validarFormulario = (event) => {
+    console.log(event.target.name);
+    switch(event.target.name){
+        case 'email':
+            if(credenciales.email_good.test(event.target.value)){
+                campos['correo'] = true;
+            } else {
+                campos['correo'] = false;
+            }
+
+            if(credenciales.email_good.test(event.target.value)){
+                campos['super_correo'] = true;
+            } else {
+                campos['super_correo'] = false;
+            }
+
+        break;
+
+        case 'password':
+            if(credenciales.password_good.test(event.target.value)){
+                campos['password'] = true;
+            } else {
+                campos['password'] = false;
+            }
+
+            if(credenciales.password_good.test(event.target.value)){
+                campos['super_password'] = true;
+            } else {
+                campos['super_password'] = false;
+            }
+
+        break;
+    }
+
+};
+
+function validarSesion(e){
+
+    if(e.target.email.value === credenciales.email){
+        campos['correo'] = true;
+    } else {
+        campos['correo'] = false;
+    }
+
+    if(e.target.password.value ===  credenciales.password){
+        campos['password'] = true;
+    } else {
+        campos['password'] = false;
+    }
+
+    if(e.target.email.value === credenciales.super_email){
+        campos['super_correo'] = true;
+    } else {
+        campos['super_correo'] = false;
+    }
+
+    if(e.target.password.value ===  credenciales.password){
+        campos['super_password'] = true;
+    } else {
+        campos['super_password'] = false;
+    }
+    
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('click', validarFormulario);
+});
+
+
+formulario.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    validarSesion(event);
+    if((campos.correo && campos.password) || (campos.super_correo && campos.super_password)){
+        Swal.fire({
+            icon: 'success',
+            title: 'Inicio exitoso',
+            text: 'Redirigiendo al inicio',
+          });
+
+          if((campos.correo && campos.password)){
+            setTimeout(() => {
+                window.location = 'inicio.html'
+            }, 3000);
+          } else if ((campos.super_correo && campos.super_password)) {
+            setTimeout(() => {
+                window.location = 'inicio-super.html'
+            }, 3000);
+          }
+          
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Credenciales invalidas',
+            text: 'Intente de nuevo',
+          });
+    }
+
+
+});
